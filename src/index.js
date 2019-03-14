dc6d8333c81c64d3c6aa7e5e09bb6f0a007fdc89
Dmitry Kulevich <dmitry.kulevich@capsilon.com>
2019-03-14 13:14:40 +0300

    feat: add implementation for function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var primeDividersMap = new Map();
+  var dividersCountInTheNumberMap = new Map();
+  var input = base;
+  var zerosCount = 0;
+
+  while (input != 1) {
+    input = getMinimalPrimeDivider(input, primeDividersMap);
+  }
+
+  for (var key of primeDividersMap.keys()) {
+    dividersCountInTheNumberMap.set(key, getCountOfDividersInNumber(number, key))
+  }
+
+  for (var key of dividersCountInTheNumberMap.keys()) {
+    console.log(key);
+    zerosCount = zerosCount > 0 && zerosCount < parseInt(dividersCountInTheNumberMap.get(key) / primeDividersMap.get(key))
+      ? zerosCount : parseInt(dividersCountInTheNumberMap.get(key) / primeDividersMap.get(key));
+  }
+
+  return zerosCount;
+}
+
+function getMinimalPrimeDivider(number, primeDividersMap) {
+  for (var i = 2; i < number + 1; i++) {
+    if (number % i == 0) {
+      if (primeDividersMap.get(i) == undefined) {
+        primeDividersMap.set(i, 1);
+      } else {
+        primeDividersMap.set(i, primeDividersMap.get(i) + 1);
+      }
+      return number / i;
+    }
+  }
+}
+
+function getCountOfDividersInNumber(number, divider) {
+  var input = number;
+  var count = 0;
+  while (input > 0) {
+    input = parseInt(input / divider);
+    count += input;
+  }
+  return count;
 }
